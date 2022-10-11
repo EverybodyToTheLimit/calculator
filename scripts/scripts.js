5c85ff655ec0af2b723a1f8d12674809df366715
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-10-11 12:24:45 +0100

    add keyboard support

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,6 +18,8 @@ function operate(operator, a, b){
     }
     }
 
+//Define empty strings
+
 let currentInput = document.getElementById('display');
 let currentOperator = "";
 let valueA = "";
@@ -111,4 +113,87 @@ document.querySelectorAll(".calc-button-operator-equal").forEach(item => {
         lastClick = "equal"
         }
     })
+
+// Add keyboard support
+
+document.addEventListener('keydown', (event) => {
+    console.log(event.key)
+    if (event.key == "0" || event.key == "1" || event.key == "2" || event.key == "3" || event.key == "4" || event.key == "5" || event.key == "6" || event.key == "7" || event.key == "8" || event.key == "9" || event.key == ".") {
+        click.currentTime = 0;
+        click.play()
+        if (currentInput.textContent.length === 15) {
+            return;
+        }
+        else if (event.key === "." && currentInput.textContent.includes(".")){
+            return;
+        }
+        else if (lastClick === "button" || lastClick === "") {
+            currentInput.textContent += event.key;
+        }
+        else {
+            currentInput.textContent = event.key;
+        }
+        lastClick = "button";
+    
+    }
+    else if (event.key == "+" || event.key == "-" || event.key == "/" || event.key == "*" || event.key == "c" || event.key == "Backspace") {
+        click.currentTime = 0;
+        click.play()
+        if (lastClick === "operator"){
+            return;
+        }
+        else if (event.key === "c") {
+            currentOperator = "";
+            valueA = "";
+            valueB = "";
+            lastClick = "";
+            currentInput.textContent = "";
+        }
+        else if (event.key === "Backspace") {
+            currentInput.textContent = currentInput.textContent.slice(0,-1);
+        }
+        else if (currentOperator != "") {
+            valueB = currentInput.textContent
+            let result = operate(currentOperator,valueA, valueB);
+            if ((Math.floor(Math.log10(result))+1) >= 15) {
+                currentInput.textContent = "NUMBER TOO BIG!"
+                return;
+            }
+            else {
+            currentInput.textContent = operate(currentOperator,valueA, valueB)
+            currentOperator = event.key;
+            valueA = currentInput.textContent;
+            valueB = ""; 
+        }
+        lastClick = "operator"
+        }
+        else {
+        currentOperator = event.key;
+        valueA = currentInput.textContent;
+        currentInput.textContent = "";
+        lastClick = "operator"
+        }
+    }
+    else if (event.key == "=") {
+
+        click.currentTime = 0;
+        click.play()
+        valueB = currentInput.textContent
+        let result = operate(currentOperator,valueA, valueB);
+        if (valueA === "") {
+            return;
+        }
+        else if ((Math.floor(Math.log10(result))+1) >= 15) {
+            currentInput.textContent = "NUMBER TOO BIG!"
+            return;
+        }
+        else {
+        valueB = currentInput.textContent
+        currentInput.textContent = operate(currentOperator,valueA, valueB)
+        valueA = currentInput.textContent; 
+        currentOperator = "";
+        lastClick = "equal"
+        }
+    }
+});
 })
